2d62c6e48cae3eca621835285b0611c174895950
Montek <87750128+Montekkundan@users.noreply.github.com>
2025-06-23 10:12:00 -0700

    ascii cube

--- src/experiments/8.ascii-knot/index.tsx
+++ src/experiments/8.ascii-knot/index.tsx
@@ -17,7 +17,7 @@ import { KnotAnimation } from './knot-animation';
 //     )
 //   }
 
-function Knot() {
+function ASCIIKnot() {
   return (
     <>
  <div className="flex w-full h-screen justify-center items-center">
@@ -27,10 +27,10 @@ function Knot() {
   )
 }
 
-Knot.Layout = DefaultLayout;
-Knot.Title = 'Knot';
-Knot.Description = 'Knot';
-Knot.Tags = ['ui', '3d'];
-Knot.background = 'dots';
+ASCIIKnot.Layout = DefaultLayout;
+ASCIIKnot.Title = 'ASCII Knot';
+ASCIIKnot.Description = 'ASCII Knot';
+ASCIIKnot.Tags = ['ui', '3d'];
+ASCIIKnot.background = 'dots';
 
-export default Knot;
+export default ASCIIKnot;
